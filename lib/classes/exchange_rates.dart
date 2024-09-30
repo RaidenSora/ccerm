b6562ea534234a12085fd89ba649315dbcd53c41
joepet <quimorajoepetryan@gmail.com>
2024-09-30 23:02:20 +0800

    Added exchange rates class

--- lib/classes/exchange_rates.dart
+++ lib/classes/exchange_rates.dart
@@ -1,1 +1,41 @@
+class ExchangeRates {
+  final Meta meta;
+  final Map<String, Currency> data;
 
+  ExchangeRates({required this.meta, required this.data});
+
+  factory ExchangeRates.fromJson(Map<String, dynamic> json) {
+    return ExchangeRates(
+      meta: Meta.fromJson(json['meta']),
+      data: (json['data'] as Map<String, dynamic>).map(
+        (key, value) => MapEntry(key, Currency.fromJson(value)),
+      ),
+    );
+  }
+}
+
+class Meta {
+  final String lastUpdatedAt;
+
+  Meta({required this.lastUpdatedAt});
+
+  factory Meta.fromJson(Map<String, dynamic> json) {
+    return Meta(
+      lastUpdatedAt: json['last_updated_at'],
+    );
+  }
+}
+
+class Currency {
+  final String code;
+  final double value;
+
+  Currency({required this.code, required this.value});
+
+  factory Currency.fromJson(Map<String, dynamic> json) {
+    return Currency(
+      code: json['code'],
+      value: double.parse(json['value'].toString()),
+    );
+  }
+}
