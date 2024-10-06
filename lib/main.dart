478a4eaf3093219e7477a08f811493fbfede10d3
joepet <quimorajoepetryan@gmail.com>
2024-10-06 23:33:48 +0800

    Add UI for exchange rates

--- lib/main.dart
+++ lib/main.dart
@@ -69,8 +69,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Timer? _debouncer;
   String? selectedValue1 = "USD";
   String? selectedValue2 = "PHP";
+  String? exchangeRatesBase = "USD";
+  String? exchangeRateAddInitialValue = "AED";
   TextEditingController amountTextField1 = TextEditingController();
   TextEditingController amountTextField2 = TextEditingController();
+  List<String> exchangeRatesRecords = ["USD", "CAD", "PHP"];
+
   //code kung san nilalagay isa isa yung value mula sa currencies.dart na file
   final List<DropdownMenuItem<String>> items =
       currencyCodes.asMap().entries.map((entry) {
@@ -256,16 +260,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   Container(
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.07),
-                            spreadRadius: 5,
-                            blurRadius: 40,
-                            offset: const Offset(0, 0),
-                          ),
-                        ]),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.07),
+                          spreadRadius: 5,
+                          blurRadius: 40,
+                          offset: const Offset(0, 0),
+                        ),
+                      ],
+                    ),
                     margin: const EdgeInsets.only(top: 70),
                     padding: const EdgeInsets.fromLTRB(25, 25, 25, 10),
                     width: MediaQuery.of(context).size.width,
@@ -296,7 +301,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           ),
                           width: MediaQuery.of(context).size.width,
                           child: DropdownButton(
-                            menuMaxHeight: 300,
+                            menuMaxHeight: 500,
                             underline: Container(),
                             iconSize: 24,
                             isExpanded: true,
@@ -351,7 +356,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           ),
                           width: MediaQuery.of(context).size.width,
                           child: DropdownButton(
-                            menuMaxHeight: 300,
+                            menuMaxHeight: 500,
                             underline: Container(),
                             iconSize: 24,
                             isExpanded: true,
@@ -392,7 +397,6 @@ class _MyHomePageState extends State<MyHomePage> {
                             decoration: const InputDecoration(
                               border: InputBorder.none,
                             ),
-                            // style: GoogleFonts.poppins(),
                           ),
                         ),
                         Container(
@@ -415,6 +419,281 @@ class _MyHomePageState extends State<MyHomePage> {
                                     "SWAP",
                                     style: GoogleFonts.poppins(
                                       color: const Color(0xff0900FF),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 40),
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
+                          child: Text(
+                            "Exchange\nRates",
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                              height: 1,
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () => showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => Dialog(
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          15, 15, 15, 5),
+                                      child: Text(
+                                        "Select Base Currency",
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.fromLTRB(
+                                          10, 10, 10, 0),
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 0, horizontal: 10),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(
+                                          color: const Color(0xFFE7E7E7),
+                                        ),
+                                      ),
+                                      width: MediaQuery.of(context).size.width,
+                                      child: DropdownButton(
+                                        menuMaxHeight: 500,
+                                        underline: Container(),
+                                        iconSize: 24,
+                                        isExpanded: true,
+                                        onChanged: (value) {
+                                          setState(() {
+                                            exchangeRatesBase = value;
+                                          });
+                                          Navigator.pop(context);
+                                        },
+                                        value: exchangeRatesBase,
+                                        items: items,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 15),
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('Close'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: const Color(0xff302C9F),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.07),
+                                  spreadRadius: 5,
+                                  blurRadius: 40,
+                                  offset: const Offset(0, 0),
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              children: [
+                                CountryFlag.fromCountryCode(
+                                  countryCodes[currencyCodes
+                                      .indexOf(exchangeRatesBase!)],
+                                  shape: const RoundedRectangle(3),
+                                  height: 23,
+                                  width: 35,
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(left: 15),
+                                  child: Text(
+                                    exchangeRatesBase!,
+                                    style: GoogleFonts.poppins(
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                ),
+                                const Spacer(),
+                                Text(
+                                  "${getCurrency(exchangeRatesBase!)} 1",
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        for (var item in exchangeRatesRecords)
+                          Container(
+                            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                            child: Row(
+                              children: [
+                                CountryFlag.fromCountryCode(
+                                  countryCodes[currencyCodes.indexOf(item)],
+                                  shape: const RoundedRectangle(3),
+                                  height: 23,
+                                  width: 35,
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(left: 15),
+                                  child: Text(
+                                    item,
+                                    style: GoogleFonts.poppins(
+                                      color: Colors.black,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                ),
+                                // Flexible(
+                                //   child: Container(
+                                //     margin: const EdgeInsets.only(left: 10),
+                                //     child: Text(
+                                //       currencyNames[
+                                //           currencyCodes.indexOf(item)],
+                                //       overflow: TextOverflow.ellipsis,
+                                //       style: GoogleFonts.poppins(
+                                //         color: const Color(0xff636363),
+                                //         fontSize: 17,
+                                //       ),
+                                //     ),
+                                //   ),
+                                // ),
+                                const Spacer(),
+                                Text(
+                                  "${getCurrency(item)} 1",
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 10),
+                          width: MediaQuery.of(context).size.width,
+                          alignment: Alignment.center,
+                          child: IconButton(
+                            onPressed: () => showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => Dialog(
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            15, 15, 15, 5),
+                                        child: Text(
+                                          "Select Currency to Add",
+                                          style: GoogleFonts.poppins(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.fromLTRB(
+                                            10, 10, 10, 0),
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 0, horizontal: 10),
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          border: Border.all(
+                                            color: const Color(0xFFE7E7E7),
+                                          ),
+                                        ),
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        child: DropdownButton(
+                                          menuMaxHeight: 500,
+                                          underline: Container(),
+                                          iconSize: 24,
+                                          isExpanded: true,
+                                          onChanged: (value) {
+                                            setState(() {
+                                              exchangeRatesRecords.add(value!);
+                                            });
+                                            Navigator.pop(context);
+                                          },
+                                          value: exchangeRateAddInitialValue,
+                                          items: items,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 15),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text('Close'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            icon: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.add_outlined,
+                                  color: Color(0xff0900FF),
+                                ),
+                                Container(
+                                  margin:
+                                      const EdgeInsets.symmetric(horizontal: 5),
+                                  child: Text(
+                                    "Add Currency",
+                                    style: GoogleFonts.poppins(
+                                      color: const Color(0xff0900FF),
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
                                 ),
