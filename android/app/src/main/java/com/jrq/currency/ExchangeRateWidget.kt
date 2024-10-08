7baa6df2fd991858753efdf34ad8f86cf67302d5
joepet <quimorajoepetryan@gmail.com>
2024-10-08 22:03:22 +0800

    Add homepage widget structure and UI

--- android/app/src/main/java/com/jrq/currency/ExchangeRateWidget.kt
+++ android/app/src/main/java/com/jrq/currency/ExchangeRateWidget.kt
@@ -5,6 +5,13 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
 import es.antonborri.home_widget.HomeWidgetPlugin
+import android.widget.ImageView
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executors
 
 /**
  * Implementation of App Widget functionality.
@@ -15,12 +22,50 @@ class ExchangeRateWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+
+        val remoteViews = RemoteViews(context.packageName, R.layout.exchange_rate_widget)
+
+        // Declaring executor to parse the URL
+        val executor = Executors.newSingleThreadExecutor()
+
+        // Once the executor parses the URL
+        // and receives the image, handler will load it
+        // in the ImageView
+        val handler = Handler(Looper.getMainLooper())
+
+        // Initializing the image
+        var image: Bitmap? = null
+
+        // Only for Background process (can take time depending on the Internet speed)
+        executor.execute {
+
+            // Image URL
+            val imageURL = "https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200x200-min.png"
+
+            // Tries to get the image and post it in the ImageView
+            // with the help of Handler
+            try {
+                val `in` = java.net.URL(imageURL).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+
+                // Only for making changes in UI
+                handler.post {
+                    remoteViews.setImageViewBitmap(R.id.baseCurrencyImageView,image )
+                }
+            }
+
+            // If the URL doesnot point to
+            // image or any other kind of failure
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             val widgetData = HomeWidgetPlugin.getData(context)
             val views = RemoteViews(context.packageName, R.layout.exchange_rate_widget)
             val textval = widgetData.getString("text","Text")
-            views.setTextViewText(R.id.appwidget_text, textval)
+            //views.setTextViewText(R.id.appwidget_text, textval)
             appWidgetManager.updateAppWidget(appWidgetId,views)
         }
     }
